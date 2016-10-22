014221e769c2722995ff6a43a5f146ccf9c97ce3
fyz <fyz_@bk.ru>
2016-10-22 17:43:45 +0300

    fix

---
+++ source/plugins.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "jquery": [
+        "dist/jquery.js"
+    ],
+    "normalize": [
+        "index.css"
+    ]
+    // "js-helpers": [
+    //     "dist/js-helpers.min.js"
+    // ]
+    // "Swiper": [
+    //     "dist/css/swiper.css",
+    //     "dist/js/swiper.jquery.js"
+    // ],
+    // "twentytwenty": [
+    //     "js/jquery.event.move.js",
+    //     "js/jquery.twentytwenty.js",
+    //     "css/twentytwenty.css"
+    // ],
+    // "jquery.scrollTo": [
+    //     "jquery.scrollTo.min.js"
+    // ],
+    // "fancybox": [
+    //     "source/jquery.fancybox.pack.js",
+    //     "source/jquery.fancybox.css",
+    //     "source/*.gif",
+    //     "source/*.png"
+    // ],
+    // "owl.carousel": [
+    //     "dist/owl.carousel.js",
+    //     "dist/assets/owl.carousel.css",
+    //     "dist/assets/owl.theme.default.css"
+    // ],
+};
